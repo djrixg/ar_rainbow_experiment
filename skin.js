37be63a8211538cc894ac7e2d8cc864132f99cf4
Ericke Gallardo <35298775+djrixg@users.noreply.github.com>
2022-02-23 10:16:07 +0800

    converted to JPG

--- skin.js
+++ skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 6.1.13/18080
 // Filename: skkk.ggsk
-// Generated 2022-02-23T10:05:06
+// Generated 2022-02-23T10:15:21
 
 function pano2vrSkin(player,base) {
 	player.addVariable('playback_video', 2, false);
